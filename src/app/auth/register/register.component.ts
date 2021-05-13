5bd284861823478a5dea44e65c191ca60b306bc0
arielvia2013 <arielvia2013@gmail.com>
2021-05-13 10:42:51 -0400

    aqui se termino lo que son los formularios de registro y  sesion  ademas que se añadio un servicio

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormControl, FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onRegister(){
+    console.log('Form->', this.registerForm.value);
   }
 
 }
