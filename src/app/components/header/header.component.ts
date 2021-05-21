df93cd3063297d8d021719ba33ddde3dc7ab2ddc
arielvia2013 <arielvia2013@gmail.com>
2021-05-21 18:12:40 -0400

    aqui se termino todo los que es la sesion el registro y el logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,32 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  providers:[AuthService]
+  providers: [AuthService]
 })
-export class HeaderComponent implements OnInit {
-  public isLogged=false;
-  public user:any;
-  constructor(private authSvc:AuthService, private router:Router) { }
+export class HeaderComponent {
 
-  async ngOnInit() {
-    //console.log('header');
-    this.user= await this.authSvc.getCurrentUser();
-    if(this.user){
-      this.isLogged=true;
-    }
+  public user$: Observable<any> = this.authSvc.afAuth.user;
+  constructor(private authSvc: AuthService, private router: Router) {
+
   }
- async onLogout(){
+
+  async onLogout() {
     try {
       await this.authSvc.logout();
       this.router.navigate(['/login']);
     } catch (error) {
       console.log(error)
     }
-    //this.authSvc.logout();
   }
 }
